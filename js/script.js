df87f7737550fbcf2b4c77bd69d282ed109ae109
Guilherme Cornacini <guilhermevcornacini@gmail.com>
2022-07-23 13:56:12 -0300

    style: created a color for all types of pokemon and modified pokedex image.

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const buttonNext = document.querySelector('.btn-next');
 let searchPokemon = 1;
 //#endregion
 
-//#region chamada API
+//#region call the API
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toString().toLowerCase()}`);
 
@@ -21,33 +21,60 @@ const fetchPokemon = async (pokemon) => {
     const dados = await APIResponse.json();
     return dados;
   }
-  return 'erro';
+  return 'error';
 }
 //#endregion
 
-//#region obtém dados da API
+//#region get data from the API
 const renderPokemon = async (pokemon) => {
   pokemonName.innerHTML = 'Loading...';
   const dados = await fetchPokemon(pokemon);
-  
-  if (dados != "erro"){
+
+  if (dados != "error"){
+    let tipoPokemon = dados.types[0].type.name;
+    let tipoPokemon2;
+    if (dados.types.length > 1)
+      tipoPokemon2 = dados.types[1].type.name;
+
     pokemonName.innerHTML = dados.name;
     pokemonId.innerHTML = dados.id + ' - ';
     pokemonType1.innerHTML = dados.types[0].type.name;
-    pokemonType2.innerHTML = dados.types[1].type.name;
+    
+    if (dados.types.length > 1)
+      pokemonType2.innerHTML = dados.types[1].type.name;
+    else {
+      pokemonType2.innerHTML = '';
+      pokemonType2.classList.remove(tipoPokemon2);
+    }
+
+    let classToDelete;
+    if (pokemonType1.classList.toString().includes('tipo')){
+      classToDelete = pokemonType1.classList[2];
+      pokemonType1.classList.remove(classToDelete);
+    }
+    if (pokemonType2.classList.toString().includes('tipo')){
+      classToDelete = pokemonType2.classList[2];
+      pokemonType2.classList.remove(classToDelete);
+    }
+
+    pokemonType1.classList.add(`tipo${tipoPokemon}`);
+    pokemonType2.classList.add(`tipo${tipoPokemon2}`);
+
     pokemonImage.src = dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
     pokemonImage.style.display = 'block';
     input.value = '';
+
     searchPokemon = dados.id;
   } else {
     pokemonImage.style.display = 'none';
-    pokemonName.innerHTML = 'Não encontrado :c';
+    pokemonName.innerHTML = 'Not found :c';
     pokemonId.innerHTML = '';
   }
 }
 //#endregion
 
-//#region Eventos
+//#region Events
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   renderPokemon(input.value);
